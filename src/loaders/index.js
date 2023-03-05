a31bcd0804226571ca68d464b76dc2cdfae3d482
rohilsaraf97 <rohilsaraf97@gmail.com>
2023-03-05 18:20:50 +0530

    fix: comment mongoose loaders

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,12 +1,12 @@
 const { logger } = require('../logger')
 const expressLoader = require('./expressLoader')
-const mongooseLoader = require('./mongooseLoader')
+// const mongooseLoader = require('./mongooseLoader')
 
 const loaders = async ({ expressApp }) => {
   await expressLoader({ app: expressApp })
   logger.info('Express Initialised.')
-  await mongooseLoader()
-  logger.info('MongoDB Initialised')
+  // await mongooseLoader()
+  // logger.info('MongoDB Initialised')
 }
 
 module.exports = loaders
